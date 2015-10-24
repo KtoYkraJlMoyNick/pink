b464cd1edca1e263dff31e9cb927c11d835789d6
KtoYkraJlMoyNick <nktrsk@gmail.com>
2015-10-24 22:50:19 +0300

    JS не туда

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function() { 
+    if (!("FormData" in window)) { 
+    return; 
+    } 
+    var form = document.querySelector(".travel-form"); 
+    form.addEventListener("submit", function(event){ 
+        event.preventDefault(); 
+        var data = new FormData(form); 
+        var xhr = new XMLHttpRequest( ); 
+        var time = (new Date( )).getTime( ); 
+        xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time); 
+        xhr.addEventListener("readystatechange", function( ) { 
+            if (xhr.readyState == 4) { 
+                console.log(xhr.responseText); 
+            } 
+            }); 
+        xhr.send(data); 
+    }) 
+})();
