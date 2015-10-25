d0dda403ab8c7811772d4ce6021f68abfbed4cc3
KtoYkraJlMoyNick <nktrsk@gmail.com>
2015-10-25 22:56:29 +0300

    Правки, куда без них

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,8 @@
     return; 
     } 
     var form = document.querySelector(".travel-form"); 
+    var successModal = document.querySelector(".modal-content--mod");
+    var closeModal = successModal.querySelector(".modal-content__link");
     form.addEventListener("submit", function(event){ 
         event.preventDefault(); 
         var data = new FormData(form); 
@@ -11,9 +13,14 @@
         xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time); 
         xhr.addEventListener("readystatechange", function( ) { 
             if (xhr.readyState == 4) { 
-                console.log(xhr.responseText); 
+					console.log(xhr.responseText);
+                    successModal.classList.add("modal-content--show"); 
                 } 
             }); 
         xhr.send(data); 
     }) 
+    closeModal.addEventListener("click", function(event){
+        event.preventDefault();
+        successModal.classList.remove("modal-content--show");
+    })
 })();
